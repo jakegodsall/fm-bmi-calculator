05e19cda5fe4598dbbb8740a3e34f65b32d5535e
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-24 14:31:22 +0000

    moved to class-based BMICalculator

--- src/main.ts
+++ src/main.ts
@@ -43,51 +43,76 @@ const weightLbsInput = document.getElementById(
   "weight-lbs"
 ) as HTMLInputElement;
 
-function getPounds(stone: number, pounds: number): number {
-  return stone * 14 + pounds;
-}
-
-function getInches(feet: number, inches: number): number {
-  return feet * 12 + inches;
-}
-
-function calculateBmi(weight: number, height: number): number {
-  return weight / (height * height);
-}
-
-function printBMI(): number {
-  let height: number;
-  let weight: number;
-
-  if (selectedRadio.id === "metric") {
-    height = parseFloat(heightCmInput.value);
-    weight = parseFloat(weightKgInput.value);
-  } else {
-    height = getInches(
-      parseFloat(heightFeetInput.value) * 12,
-      parseFloat(heightInchesInput.value)
-    );
-    weight = getPounds(
-      parseFloat(weightStoneInput.value) * 14,
-      parseFloat(weightLbsInput.value)
-    );
+class BMICalculator {
+  getPoundsFromStoneAndPounds(stone: number, pounds: number): number {
+    return stone * 14 + pounds;
   }
 
-  let bmi: number = 0;
-
-  if (!isNaN(height) && !isNaN(weight)) {
-    bmi = calculateBmi(height, weight);
+  getInchesFromFeetAndInches(feet: number, inches: number): number {
+    return feet * 12 + inches;
   }
 
-  console.log("height: " + height);
-  console.log("weight: " + weight);
+  calculateBmi(weight: number, height: number): number {
+    return weight / (height * height);
+  }
 
-  return bmi;
+  printBMI(): number {
+    let height: number;
+    let weight: number;
+
+    if (selectedRadio.id === "metric") {
+      height = parseFloat(heightCmInput.value);
+      weight = parseFloat(weightKgInput.value);
+
+      console.log("height in cm: " + height);
+      console.log("weight in kg: " + weight);
+    } else {
+      height = this.getInchesFromFeetAndInches(
+        parseFloat(heightFeetInput.value),
+        parseFloat(heightInchesInput.value)
+      );
+      console.log("height:" + height);
+    }
+
+    return 1;
+  }
 }
 
-heightCmInput.addEventListener("change", printBMI);
-weightKgInput.addEventListener("change", printBMI);
-heightFeetInput.addEventListener("change", printBMI);
-heightInchesInput.addEventListener("change", printBMI);
-weightStoneInput.addEventListener("change", printBMI);
-weightLbsInput.addEventListener("change", printBMI);
+const bmiCalculator = new BMICalculator();
+
+// function printBMI(): number {
+//   let height: number;
+//   let weight: number;
+
+//   if (selectedRadio.id === "metric") {
+//     height = parseFloat(heightCmInput.value);
+//     weight = parseFloat(weightKgInput.value);
+//   } else {
+//     height = getInches(
+//       parseFloat(heightFeetInput.value) * 12,
+//       parseFloat(heightInchesInput.value)
+//     );
+//     weight = getPounds(
+//       parseFloat(weightStoneInput.value) * 14,
+//       parseFloat(weightLbsInput.value)
+//     );
+//   }
+
+//   let bmi: number = 0;
+
+//   if (!isNaN(height) && !isNaN(weight)) {
+//     bmi = calculateBmi(height, weight);
+//   }
+
+//   console.log("height: " + height);
+//   console.log("weight: " + weight);
+
+//   return bmi;
+// }
+
+heightCmInput.addEventListener("change", bmiCalculator.printBMI);
+weightKgInput.addEventListener("change", bmiCalculator.printBMI);
+heightFeetInput.addEventListener("change", bmiCalculator.printBMI);
+heightInchesInput.addEventListener("change", bmiCalculator.printBMI);
+weightStoneInput.addEventListener("change", bmiCalculator.printBMI);
+weightLbsInput.addEventListener("change", bmiCalculator.printBMI);
